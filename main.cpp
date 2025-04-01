a8e915db56e6ce4af5beb51b5147b00c62a0547c
AlexanderToles <alexandertoleseducation@gmail.com>
2025-04-01 13:42:59 -0500

    test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include "matrix.hpp"
 
 int main(int argc, char *argv[]) {
+    Matrix matrix({
+        { 25, 35, 45 },
+        { 15, 45, 45 },
+        { 80, 80, 80 },
+    });
     return 0;
 }
