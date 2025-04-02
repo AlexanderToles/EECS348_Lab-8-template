ad57fc060b7638a4ec1f20e3410a8ea4ee8859bd
AlexanderToles <alexandertoleseducation@gmail.com>
2025-04-01 19:54:56 -0500

    test

--- matrix.cpp
+++ matrix.cpp
@@ -5,13 +5,13 @@
 // Constructor to initialize an N x N matrix with zeroes
 Matrix::Matrix(std::size_t N) {
     size = N;
-    newMatrix.resize(N, std::vector<int>(N, 0)); // Initialize matrix with zeros
+    matrix.resize(N, std::vector<int>(N, 0)); // Initialize matrix with zeros
 }
 
 // Constructor to initialize the matrix with a given 2D vector
 Matrix::Matrix(std::vector<std::vector<int>> nums) {
     size = nums.size();
-    newMatrix = nums; // Directly initialize the matrix with the provided vector
+    matrix = nums; // Directly initialize the matrix with the provided vector
 }
 
 // Matrix addition
@@ -19,7 +19,7 @@ Matrix Matrix::operator+(const Matrix &rhs) const {
     Matrix result(size);
     for (std::size_t i = 0; i < size; i++) {
         for (std::size_t j = 0; j < size; j++) {
-            result.newMatrix[i][j] = newMatrix[i][j] + rhs.newMatrix[i][j];
+            result.matrix[i][j] = matrix[i][j] + rhs.matrix[i][j];
         }
     }
     return result;
@@ -31,7 +31,7 @@ Matrix Matrix::operator*(const Matrix &rhs) const {
     for (std::size_t i = 0; i < size; i++) {
         for (std::size_t j = 0; j < size; j++) {
             for (std::size_t k = 0; k < size; k++) {
-                result.newMatrix[i][j] += newMatrix[i][k] * rhs.newMatrix[k][j];
+                result.matrix[i][j] += matrix[i][k] * rhs.matrix[k][j];
             }
         }
     }
@@ -40,12 +40,12 @@ Matrix Matrix::operator*(const Matrix &rhs) const {
 
 // Set value at position (i, j)
 void Matrix::set_value(std::size_t i, std::size_t j, int n) {
-    newMatrix[i][j] = n;
+    matrix[i][j] = n;
 }
 
 // Get value at position (i, j)
 int Matrix::get_value(std::size_t i, std::size_t j) const {
-    return newMatrix[i][j];
+    return matrix[i][j];
 }
 
 // Get the size of the matrix (number of rows or columns)
@@ -57,7 +57,7 @@ int Matrix::get_size() const {
 int Matrix::sum_diagonal_major() const {
     int sum = 0;
     for (std::size_t i = 0; i < size; i++) {
-        sum += newMatrix[i][i];  // Diagonal element (i, i)
+        sum += matrix[i][i];  // Diagonal element (i, i)
     }
     return sum;
 }
@@ -66,26 +66,26 @@ int Matrix::sum_diagonal_major() const {
 int Matrix::sum_diagonal_minor() const {
     int sum = 0;
     for (std::size_t i = 0; i < size; i++) {
-        sum += newMatrix[i][size - 1 - i];  // Minor diagonal element (i, size-i-1)
+        sum += matrix[i][size - 1 - i];  // Minor diagonal element (i, size-i-1)
     }
     return sum;
 }
 
 // Swap two rows (r1 and r2)
 void Matrix::swap_rows(std::size_t r1, std::size_t r2) {
-    std::swap(newMatrix[r1], newMatrix[r2]);
+    std::swap(matrix[r1], matrix[r2]);
 }
 
 // Swap two columns (c1 and c2)
 void Matrix::swap_cols(std::size_t c1, std::size_t c2) {
     for (std::size_t i = 0; i < size; i++) {
-        std::swap(newMatrix[i][c1], newMatrix[i][c2]);
+        std::swap(matrix[i][c1], matrix[i][c2]);
     }
 }
 
 // Print the matrix
 void Matrix::print_matrix() const {
-    for (const auto &row : newMatrix) {
+    for (const auto &row : matrix) {
         for (int num : row) {
             std::cout << num << " ";
         }
